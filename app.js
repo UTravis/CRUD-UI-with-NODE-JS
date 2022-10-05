dae24331efcd822653d12acb42d263d1f8bb13a1
Travis Uche Emeka <ucheemeka666.ue@gmail.com>
2022-10-05 15:45:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const profileModel = require('./model/profileModel');
 const parser = require('body-parser')
 
-const database = "mongodb+srv://travis:12345@cluster0.a6gdicd.mongodb.net/?retryWrites=true&w=majority"
+const database = "mongodb+srv://username:password@cluster0.a6gdicd.mongodb.net/?retryWrites=true&w=majority"
 const port = 3000;
 const app = express()
 
